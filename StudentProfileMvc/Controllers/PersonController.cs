b570226316609ec5a9b27ae103ef99a62119ea17
aryaraju28 <arya28199@gmail.com>
2021-04-18 18:50:41 +0530

    Add project files.

---
+++ StudentProfileMvc/Controllers/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using StudentProfileMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentProfileMvc.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        public ActionResult Registration1(Person P)
+        {
+            return View("About", P);
+        }
+       
+
+    }
+}
---
+++ StudentProfileMvc/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentProfileMvc.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Education { get; set; }
+        public string Ug { get; set; }
+        public bool Singing { get; set; }
+        public bool Dancing { get; set; }
+        public bool Hobbies { get; set; }
+    }
+}
